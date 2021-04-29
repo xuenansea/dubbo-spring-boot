ab0062cbae8fe7a2251e26ce31ac14ecf2102392
xuenansea <isxuenan@163.com>
2021-04-29 20:44:33 +0800

    dev

--- dubbo-demo/dubbo-api/src/main/java/com/learn/dubbo/DubboApiApplication.java
+++ dubbo-demo/dubbo-api/src/main/java/com/learn/dubbo/DubboApiApplication.java
@@ -1,11 +1,11 @@
 package com.learn.dubbo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class DubboApiApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DubboApiApplication.class, args);
-    }
+//    public static void main(String[] args) {
+//        SpringApplication.run(DubboApiApplication.class, args);
+//    }
 }
